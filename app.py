379a46df6f72f9a77e6a8a063b376e337a8a5281
KullapornPromlachai <kullaporn.plc@gmail.com>
2025-04-20 16:39:30 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+
+
+#app.py
+import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+#Load model
+with open ('kmeans_model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+    
+
+#Set title
+st.title("K-Means Clustering Visualizer by Kullaporn Promlachai")
+
+#Set the page config
+st.set_page_config(page_title= "K-Means Clustering", layout = "centered")
+
+#Load from a saved dataset or generate synthetic data
+X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+
+#Predict using the loaded model
+y_kmeans = loaded_model.predict(X)
+
+
+#Display cluster centers
+st.subheader("Example Data for Visualization")
+st.markdown("This demo uses example data (2D) to illustrate clustering results.")
+
+#plotting
+fig, ax = plt.subplots()
+scatter = ax.scatter(X[:,0], X[:,1], c=y_kmeans, cmap = 'viridis')
+ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
+ax.set_title('k-Means Clustering')
+ax.legend()
+st.pyplot(fig)
